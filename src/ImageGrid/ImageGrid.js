327f077bb9898a883af35daeda45a721ca2cad56
Julie Broderick <broderick.julie@gmail.com>
2019-04-08 16:04:25 -0500

    grid styles

--- src/ImageGrid/ImageGrid.js
+++ src/ImageGrid/ImageGrid.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import './ImageGrid.css';
+import "./ImageGrid.css";
 
 export default class ImageGrid extends Component {
-    static defaultProps = {
-        images: []
-    }
-    
-    render() {
-    const images = this.props.images.map(img => <img className="images" src={img.url} alt="placeholder" key={img.id}/>);
+  static defaultProps = {
+    images: []
+  };
 
-    return <div className="image-grid">{images}</div>;
+  render() {
+    const { images } = this.props;
+
+    const gridItems = images.map(img => (
+      <img className="grid-item" src={img.url} alt="placeholder" key={img.id} />
+    ));
+
+    return <div className="grid">{gridItems}</div>;
   }
 }
